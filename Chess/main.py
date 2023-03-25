b0053b080f6d2bc181f1c51e1868b0a631defadf
GerasimNotFound <son_of_a_sin@mail.ru>
2023-03-25 20:52:42 +0300

    Done Board

--- Chess/main.py
+++ Chess/main.py
@@ -9,7 +9,17 @@ FPS = 15
 IMAGES = {}
 LIGHTBLUE = (132,196,192)
 BLUE = (0,86,143)
+WHITE = (255,255,255)
 BACKGROUND = (230,96,114)
+LTRS = 'АБВГДЕЖЗИЙКЛМНОПРСТ'
+COLLORS = [BLUE, LIGHTBLUE]
+n_lines = p.Surface((SQ_SIZE * SQ_SIZE, SQ_SIZE // 2))
+n_rows = p.Surface((SQ_SIZE // 2, DIMENSION * SQ_SIZE))
+fields = p.Surface((DIMENSION * SQ_SIZE, DIMENSION * SQ_SIZE))
+boardSur = p.Surface((
+    2*n_rows.get_width() + fields.get_width(),
+    2*n_lines.get_height() + fields.get_height()
+))
 
 def load_Images():
     pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
@@ -21,8 +31,8 @@ def load_Images():
 def main():
     p.init()
     screen = p.display.set_mode(WIN_SIZE)
+    screen.fill(BACKGROUND)
     clock = p.time.Clock()
-    screen.fill(p.Color(255,255,255))
     gs = ChessEngine.GameState()
     load_Images()
     running = True
@@ -35,28 +45,64 @@ def main():
         p.display.flip()
 
 def DrawGameState(screen, gs):
+    bliting(screen, gs)
     DrawBoard(screen)
-    DrawPieces(screen, gs.board)
+    DrawFont(screen)
 
 
 def DrawBoard(screen):
-    colors = [p.Color(LIGHTBLUE), p.Color(BLUE)]
-    for r in range(DIMENSION):
-        for c in range(DIMENSION):
-            color = colors[((r+c)%2)]
-            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE ))
+    is_even_qty = (DIMENSION % 2 == 0)
+    cell_color_index = 1 if (is_even_qty) else 0
+    for y in range(DIMENSION):
+        for x in range(DIMENSION):
+            cell = p.Surface((SQ_SIZE, SQ_SIZE))
+            cell.fill(COLLORS[cell_color_index])
+            fields.blit(cell, (x * SQ_SIZE, y * SQ_SIZE))
+            cell_color_index ^= True
+        cell_color_index = cell_color_index ^ True if (is_even_qty) else cell_color_index
 
 def DrawFont(screen):
-    pass
+    FNT18 = p.font.Font(p.font.get_default_font(), 16)
+    for i in range(0, DIMENSION):
+        letter = FNT18.render(LTRS[i],1,WHITE)
+        number = FNT18.render(str(DIMENSION - i), 1, WHITE)
+        n_lines.blit(letter, (
+            i * SQ_SIZE + (SQ_SIZE - letter.get_rect().width) // 2,
+            (n_lines.get_height() - letter.get_rect().height) // 2
+        ))
+        n_rows.blit(number, (
+            (n_rows.get_width() - letter.get_rect().width) // 2,
+            i * SQ_SIZE + (SQ_SIZE - number.get_rect().height) // 2
+        ))
 
-def DrawPieces(screen, board):
+def DrawPieces(fields, board):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             piece = board[r][c]
             if piece != "--":
-                screen.blit(IMAGES[piece], p.Rect(c* SQ_SIZE, r* SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                fields.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
+def bliting(screen, gs):
+    DrawPieces(fields, gs.board)
+    boardSur.blit(n_rows, (0, n_lines.get_height()))
+    boardSur.blit(n_rows, (n_rows.get_width() + fields.get_width(), n_lines.get_height()))
+    boardSur.blit(n_lines, (n_rows.get_width(), 0))
+    boardSur.blit(n_lines, (n_rows.get_width(), n_rows.get_width() + fields.get_width()))
+    boardSur.blit(fields, (n_rows.get_width(), n_lines.get_height()))
+    screen.blit(boardSur, (
+        (WIN_SIZE[0] - boardSur.get_width()) // 2,
+        (WIN_SIZE[1] - boardSur.get_height()) // 2
+    ))
+    
+
+# def DrawPieces(fields, board):
+#     for r in range(DIMENSION):
+#         for c in range(DIMENSION):
+#             piece = board[r][c]
+#             if piece != "--":
+#                 fields.blit(IMAGES[piece], p.Rect(c* SQ_SIZE, r* SQ_SIZE, SQ_SIZE, SQ_SIZE))
+#
 
 if __name__ == "__main__":
     main()
